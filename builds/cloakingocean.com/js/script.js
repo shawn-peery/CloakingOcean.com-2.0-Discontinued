f03b630844d6ca1d73170f52586b846d05ed1b6a
BuildTools <unconfigured@null.spigotmc.org>
2018-10-22 12:40:12 -0600

    Successfully made the header modular using jquery

--- builds/cloakingocean.com/js/script.js
+++ builds/cloakingocean.com/js/script.js
@@ -1,43 +1,40 @@
-alert("Version 4!");
+var links = [
+    {url: "../index.html", name: "Home"},
+    {url: "../java.html", name: "Java"},
+    {url: "../web.html", name: "Web"},
+    {url: "../unity.html", name: "Unity"},
+    {url: "../gaming.html", name: "Gaming"},
+    {url: "../Music", name: "Music"},
+];
 
-var $header = $(("<nav class='navbar bg-dark navbar-dark navbar-expand-sm'>" +
-    "<div class='container-fluid'>" +
-        "<div class='navbar-header'>" +
-            "<a href='index.html'><img class='logo navbar-brand' src='images/logo/logo-white.svg'></a>" +
-        "</div>" +
-    "</div>" +
-    "<div class='container-fluid' style='width: 100vw;'>" +
-        "<div class='navbar-nav justify-content-end ml-auto mr-auto ml-sm-auto mr-sm-0'>" +
-            "<a class='nav-item nav-link' href='#'>Home</a>" +
-            "<a class='nav-item nav-link' href='#'>Java</a>" +
-            "<a class='nav-item nav-link' href='#'>Web</a>" +
-            "<a class='nav-item nav-link' href='#'>Unity</a>" +
-            "<a class='nav-item nav-link' href='#'>Gaming</a>" +
-            "<a class='nav-item nav-link' href='#'>Music</a>" +
-        "</div><!-- navbar-nav -->" +
-    "</div><!-- container-fluid -->" +
-"</nav><!-- nav-bar -->"));
-$header.insertAfter("header");
-// var $name = $("header").html();
-// alert($name);
+var currentIndex = $("header").data("id");
 
-/**
-<nav class="navbar bg-dark navbar-dark navbar-expand-sm">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a href="index.html"><img class="logo navbar-brand" src="images/logo/logo-white.svg"></a>
-        </div>
-    </div>
-    <div class="container-fluid" style="width: 100vw;">
-        <div class="navbar-nav justify-content-end ml-auto mr-auto ml-sm-auto mr-sm-0">
-            <a class="nav-item nav-link" href="#">Home</a>
-            <a class="nav-item nav-link" href="#">Java</a>
-            <a class="nav-item nav-link" href="#">Web</a>
-            <a class="nav-item nav-link" href="#">Unity</a>
-            <a class="nav-item nav-link" href="#">Gaming</a>
-            <a class="nav-item nav-link" href="#">Music</a>
-        </div><!-- navbar-nav -->
-    </div><!-- container-fluid -->
-</nav><!-- nav-bar -->
+writeHeader();
+alert("test");
+function writeHeader() {
+    var $headerString = "<nav class='navbar bg-dark navbar-dark navbar-expand-sm'>" +
+                        "<div class='container-fluid'>" +
+                            "<div class='navbar-header'>" +
+                                "<a href='index.html'><img class='logo navbar-brand' src='images/logo/logo-white.svg'></a>" +
+                            "</div>" +
+                        "</div>" +
+                        "<div class='container-fluid' style='width: 100vw;'>" +
+                            "<div class='navbar-nav justify-content-end ml-auto mr-auto ml-sm-auto mr-sm-0'>";
+    for (let i = 0; i < links.length; i++) {
+        let url = links[i].url;
+        let name = links[i].name;
 
-*/
+        if (i == currentIndex) {
+            $headerString += "<a class='nav-item nav-link disabled' href='#'>" + name +  "</a>";
+        } else {
+            $headerString += "<a class='nav-item nav-link' href='" + url + "'>" + name +  "</a>";
+        }
+    }
+
+    $headerString +=        "</div><!-- navbar-nav -->" +
+                        "</div><!-- container-fluid -->" +
+                    "</nav><!-- nav-bar -->"
+
+    var $header = $($headerString);
+    $header.insertAfter("header");
+}
