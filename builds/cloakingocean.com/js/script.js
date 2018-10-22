751793add0745e12402904b47da02ea7e2eab748
BuildTools <unconfigured@null.spigotmc.org>
2018-10-22 12:54:24 -0600

    Quick fix to put the html inside the header rather than after it

--- builds/cloakingocean.com/js/script.js
+++ builds/cloakingocean.com/js/script.js
@@ -4,37 +4,45 @@ var links = [
     {url: "../web.html", name: "Web"},
     {url: "../unity.html", name: "Unity"},
     {url: "../gaming.html", name: "Gaming"},
-    {url: "../Music", name: "Music"},
+    {url: "../music", name: "Music"},
 ];
 
 var currentIndex = $("header").data("id");
 
 writeHeader();
-alert("test");
+
 function writeHeader() {
-    var $headerString = "<nav class='navbar bg-dark navbar-dark navbar-expand-sm'>" +
-                        "<div class='container-fluid'>" +
-                            "<div class='navbar-header'>" +
-                                "<a href='index.html'><img class='logo navbar-brand' src='images/logo/logo-white.svg'></a>" +
-                            "</div>" +
-                        "</div>" +
-                        "<div class='container-fluid' style='width: 100vw;'>" +
-                            "<div class='navbar-nav justify-content-end ml-auto mr-auto ml-sm-auto mr-sm-0'>";
+    let $headerString = "" +
+    "<nav class='navbar bg-dark navbar-dark navbar-expand-sm'>" +
+        "<div class='container-fluid'>" +
+            "<div class='navbar-header'>" +
+                "<a href='index.html'><img class='logo navbar-brand' src='images/logo/logo-white.svg'></a>" +
+            "</div>" +
+        "</div>" +
+        "<div class='container-fluid' style='width: 100vw;'>" +
+            "<div class='navbar-nav justify-content-end ml-auto mr-auto ml-sm-auto mr-sm-0'>";
+
+    $headerString += addLinksForEachPage() +
+
+            "</div><!-- navbar-nav -->"
+        "</div><!-- container-fluid -->" +
+    "</nav><!-- nav-bar -->"
+
+    let $header = $($headerString);
+    $("header").html($header);
+}
+
+function addLinksForEachPage() {
+    let $string = "";
     for (let i = 0; i < links.length; i++) {
         let url = links[i].url;
         let name = links[i].name;
 
         if (i == currentIndex) {
-            $headerString += "<a class='nav-item nav-link disabled' href='#'>" + name +  "</a>";
+            $string += "<a class='nav-item nav-link disabled' href='#'>" + name +  "</a>";
         } else {
-            $headerString += "<a class='nav-item nav-link' href='" + url + "'>" + name +  "</a>";
+            $string += "<a class='nav-item nav-link' href='" + url + "'>" + name +  "</a>";
         }
     }
-
-    $headerString +=        "</div><!-- navbar-nav -->" +
-                        "</div><!-- container-fluid -->" +
-                    "</nav><!-- nav-bar -->"
-
-    var $header = $($headerString);
-    $header.insertAfter("header");
+    return $string;
 }
